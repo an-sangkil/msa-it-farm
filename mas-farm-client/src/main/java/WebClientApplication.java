e95fe2a35781e5e18364ee79b4f4809ee4220b16
sangkil.an <mycup@nate.com>
2020-06-04 10:57:57 +0900

    최초 프레임웍 생성

---
+++ mas-farm-client/src/main/java/WebClientApplication.java
@@ -0,0 +1,13 @@
+/**
+ * <pre>
+ * Description :
+ *
+ *
+ * </pre>
+ *
+ * @author skan
+ * @version Copyright (C) 2020 by CJENM|Mezzomedia. All right reserved.
+ * @since 2020-06-04
+ */
+public class WebClientApplication {
+}
---
+++ mas-farm-client/src/test/java/WebClientApplicationTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <pre>
+ * Description :
+ *
+ *
+ * </pre>
+ *
+ * @author skan
+ * @version Copyright (C) 2020 by CJENM|Mezzomedia. All right reserved.
+ * @since 2020-06-04
+ */
+class WebClientApplicationTest {
+
+}
