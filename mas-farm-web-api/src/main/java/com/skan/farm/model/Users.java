3334d3caeffc4f33d1c8d9c70b66017f09db8a7a
sangkil.an <mycup@nate.com>
2020-07-06 11:31:54 +0900

    기본 도메인 추가  / cattle entit y 설정

--- mas-farm-web-api/src/main/java/com/skan/farm/model/Users.java
+++ mas-farm-web-api/src/main/java/com/skan/farm/model/Users.java
@@ -1,6 +1,5 @@
 package com.skan.farm.model;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,7 +7,6 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
